c3c3fb1915b65828537d8bcdfd1bba4c9febe744
mohitt13 <mtilokchandani@gmail.com>
2025-05-02 16:50:31 +0530

    Kafka Project setup : Mohit

---
+++ src/main/java/com/example/kafkaConnectPrac/KafkaConnectPracApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafkaConnectPrac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConnectPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConnectPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kafkaConnectPrac/KafkaConnectPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkaConnectPrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConnectPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
